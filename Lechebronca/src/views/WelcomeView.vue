1a1627c570977d48474726beb2a7d3a957164059
Jordix69 <laracisnerosjairo@gmail.com>
2025-06-14 15:31:38 -0600

    press start

--- Lechebronca/src/views/WelcomeView.vue
+++ Lechebronca/src/views/WelcomeView.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="gif-background">
+  <div class="gifprin">
+    <img src="@/assets/presstart.gif" class="enter" />
   </div>
 </template>
 
 <style scoped>
-.gif-background {
+.gifprin {
   height: 100vh;
   width: 100vw;
   background-image: url('@/assets/rainbow.gif');
   background-repeat: repeat;
   background-position: center;
+  position: relative;
+  overflow: hidden;
+}
+
+.enter {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background-image: url('@/assets/rainbow.gif');
+  border: solid black 2px;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: auto;
+  z-index: 1;
 }
 </style>
